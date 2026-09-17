8df8cdedf308ce660037ab484be83ff82b698217
Touhid Tamim <194215315+touhidtamim@users.noreply.github.com>
2026-09-17 21:48:01 +0600

    Add main layout

--- frontend/src/layouts/MainLayout.jsx
+++ frontend/src/layouts/MainLayout.jsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import React from "react";
+import Navbar from "./../components/navbar/Navbar";
+import Footer from "./../components/footer/Footer";
+import { Outlet } from "react-router";
 
 const MainLayout = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      {/* Navbar */}
+      <Navbar />
+
+      {/* Dynamic component*/}
+      <div className="w-11/12 mx-auto min-h-screen">
+        <Outlet />
+      </div>
+      {/* Footer */}
+      <Footer />
+    </>
   );
 };
 
--- frontend/src/router/PublicRoute.jsx
+++ frontend/src/router/PublicRoute.jsx
@@ -1,9 +1,10 @@
 import { createBrowserRouter } from "react-router";
+import MainLayout from "../layouts/MainLayout";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello World</div>,
+    element: <MainLayout />,
   },
 ]);
 
